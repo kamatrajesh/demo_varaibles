d8aa64552aac9b0529229fc3315ac8eef71114df
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-04 07:14:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+   region     = "eu-central-1"
+   access_key = "AKIATQ37NXB2AYK7R6PQ"
+   secret_key = "S1Yg1Qm2JNSej8EHdhPTiu5l5ZD36URsedf32kNT"
+}
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  var.instance_type
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+} 
